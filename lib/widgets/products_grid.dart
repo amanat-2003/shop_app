f20db8f9a249120ffafcfbd1da58e9dee9d8dd22
amanat-2003 <amanatsinghnain@gmail.com>
2022-10-01 10:50:09 +0530

    lec194 completed

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import './product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ProductItem(
+        id: products[i].id,
+        title: products[i].title,
+        ImageUrl: products[i].imageUrl,
+        price: products[i].price,
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 2 / 3,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
