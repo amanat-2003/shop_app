c08c9824f6dfac93330736db2d7a9f9524f4e3ed
amanat-2003 <amanatsinghnain@gmail.com>
2022-10-11 10:23:04 +0530

    lec252 completed

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -126,9 +126,18 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = Uri.parse(
+          'https://flutter-app-6ab90-default-rtdb.firebaseio.com/products/$id.json');
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'price': newProduct.price,
+            'imageUrl': newProduct.imageUrl,
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
