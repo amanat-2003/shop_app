61cdb82522ad66bf318fda362ea05bc3f4795295
amanat-2003 <amanatsinghnain@gmail.com>
2022-10-11 11:58:38 +0530

    completed lec255

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,7 +20,12 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavouriteStatus() {
+  Future<void> toggleFavouriteStatus() async {
+    final url = Uri.parse(
+        'https://flutter-app-6ab90-default-rtdb.firebaseio.com/products/$id.json');
+    await http.patch(url, body: json.encode({
+      'isFavourite':!isFavourite
+    }));
     isFavourite = !isFavourite;
     notifyListeners();
   }
