6803fccb27a03291efe407dde4df640a1fe0e870
Amanat Singh <amanatsinghnain@gmail.com>
2022-12-27 21:56:01 +0530

    lec289 completed

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -13,8 +13,29 @@ class OrderItem extends StatefulWidget {
   State<OrderItem> createState() => _OrderItemState();
 }
 
-class _OrderItemState extends State<OrderItem> {
+class _OrderItemState extends State<OrderItem>
+    with SingleTickerProviderStateMixin {
   var _expanded = false;
+  AnimationController _controller;
+  Animation<double> _opacityAnimation;
+  Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _opacityAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    ));
+    _slideAnimation = Tween<Offset>(begin: Offset(-2, -2), end: Offset(0, 0))
+        .animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.bounceIn,
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,41 +51,58 @@ class _OrderItemState extends State<OrderItem> {
               trailing: IconButton(
                 icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
                 onPressed: () {
+                  if (_expanded == false)
+                    _controller.forward();
+                  else
+                    _controller.reverse();
                   setState(() {
                     _expanded = !_expanded;
                   });
                 },
               ),
             ),
-            if (_expanded)
-              Container(
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              curve: Curves.easeIn,
+              height: _expanded
+                  ? min(widget.order.products.length * 20.0 + 10, 100.0)
+                  : 0,
+              child: Container(
                 padding: EdgeInsets.symmetric(
                   horizontal: 15,
                   vertical: 4,
                 ),
-                height: min(widget.order.products.length * 20.0 + 10, 100.0),
+                // height: min(widget.order.products.length * 20.0 + 10, 100.0),
                 child: ListView(
                   children: widget.order.products
-                      .map((prod) => Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                prod.title,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                      .map((prod) => FadeTransition(
+                            opacity: _opacityAnimation,
+                            child: SlideTransition(
+                              position: _slideAnimation,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    prod.title,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    "${prod.quantity} x \$${prod.price}",
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.grey),
+                                  ),
+                                ],
                               ),
-                              Text(
-                                "${prod.quantity} x \$${prod.price}",
-                                style:
-                                    TextStyle(fontSize: 18, color: Colors.grey),
-                              ),
-                            ],
+                            ),
                           ))
                       .toList(),
                 ),
-              )
+              ),
+            )
           ],
         ));
   }
